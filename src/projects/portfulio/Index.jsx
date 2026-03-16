74824b6329e8ac00ffebb966296217a4ba486ca8
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-03-17 00:36:26 +0600

    basic portfulio setup

---
+++ src/projects/portfulio/Index.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import Introduction from './components/Introduction';
+
+const Index = () => {
+    return (
+        <div className="container text-white mx-auto">
+            <Header />
+            <Introduction />
+        </div>
+    )
+}
+
+export default Index;
